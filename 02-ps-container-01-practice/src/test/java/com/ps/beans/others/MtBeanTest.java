28394865e9ae1a5946fa4b52b98c911a23877ef6
Victor Kokachev <Victor_Kokachev@epam.com>
2018-05-30 17:28:24 +0300

    splitting config

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/others/MtBeanTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/others/MtBeanTest.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -24,5 +25,14 @@ public class MtBeanTest {
         MultipleTypesBean mtBean = (MultipleTypesBean) ctx.getBean("mtBean");
         assertNotNull(mtBean);
 
+        int beanCount1 = ctx.getBeanDefinitionNames().length;
+
+        ApplicationContext ctx2 = new ClassPathXmlApplicationContext(
+                "classpath:spring/others/*-config.xml"
+        );
+
+        int beanCount2 = ctx2.getBeanDefinitionNames().length;
+
+        assertEquals(beanCount1, beanCount2);
     }
 }
